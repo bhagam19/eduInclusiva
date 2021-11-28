27399396b3649981b88d3d509bc503e671ffaf56
DrRH <dorianrodrigo@gmail.com>
2021-11-28 02:45:56 -0500

    Se deja funcional la pestaña <2 Entorno Salud> hasta la fila horarios

--- adm/03-cnt/03-funciones/extraerTextos.php
+++ adm/03-cnt/03-funciones/extraerTextos.php
@@ -5,12 +5,12 @@
     $medicaments[$m]=$medicamentos[$n];
     $horaris[$m][$k]=$horario[$n];
     $comp=$medicamentos[$n];
-    while($n<count($medicamentos)){
+    while($n<count($medicamentos)-1){
         $n++;
-        echo '<br> n:'.$n.' m:'.$m.' k:'.$k.'   '.$medicaments[$m].'---->'.$horaris[$m][$k].'<br>';
         if(strcmp($medicamentos[$n], $comp)==0){
-            $horaris[$m][$k]=$horario[$n];
             $k++;
+            $horaris[$m][$k]=$horario[$n];
+            $comp=$medicamentos[$n];
         }else{
             $m++;
             $medicaments[$m]=$medicamentos[$n];
