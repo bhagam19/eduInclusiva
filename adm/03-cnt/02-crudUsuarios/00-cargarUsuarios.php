bdcc53aa3000cdca3d3cac0afd4d46acfaf59fba
bhagam19 <bhagam19@gmail.com>
2021-12-12 17:28:01 -0500

    Se actualizó la presentación del crudUsuarios. Se fijo las primera columnas y el encabezado.

--- adm/03-cnt/02-crudUsuarios/00-cargarUsuarios.php
+++ adm/03-cnt/02-crudUsuarios/00-cargarUsuarios.php
@@ -18,15 +18,15 @@
 	}
 	$respuesta="";
 	$respuesta.='	
-		<tr>									
-			<td>Nuevo:</td>
+		<tr class="stickyHead3">							
+			<td class="sticky1">Nuevo:</td>
+			<td class="sticky2"><input type="text" name"nombres" id="nombres" style="width:120px" onkeyup="cambiarFondoInput(this.id)"></td>
+			<td><input type="text" name"apellidos" id="apellidos" style="width:120px" onkeyup="cambiarFondoInput(this.id)"></td>
 			<td><input class="submit upload" type="file" name"foto" id="foto" style="width:120px"></td>
-			<td><input type="text" name"usuarioCEDU" id="usuarioCEDU" style="width:150px" onkeyup="cambiarFondoInput(this.id)"></td>
-			<td><input type="text" name"nombres" id="nombres" style="width:150px" onkeyup="cambiarFondoInput(this.id)"></td>
-			<td><input type="text" name"apellidos" id="apellidos" style="width:150px" onkeyup="cambiarFondoInput(this.id)"></td>
-			<td><input type="text" name"correo" id="correo" style="width:150px" onkeyup="cambiarFondoInput(this.id)"></td>
-			<td><input type="text" name"usuario" id="usuario" style="width:150px" onkeyup="cambiarFondoInput(this.id)"></td>
-			<td><input type="password" name"contrasena" id="contrasena" style="width:150px" onkeyup="cambiarFondoInput(this.id)"></td>
+			<td><input type="text" name"usuarioCEDU" id="usuarioCEDU" style="width:120px" onkeyup="cambiarFondoInput(this.id)"></td>
+			<td><input type="text" name"correo" id="correo" style="width:120px" onkeyup="cambiarFondoInput(this.id)"></td>
+			<td><input type="text" name"usuario" id="usuario" style="width:120px" onkeyup="cambiarFondoInput(this.id)"></td>
+			<td><input type="password" name"contrasena" id="contrasena" style="width:120px" onkeyup="cambiarFondoInput(this.id)"></td>
 			<td><select name="defUsuario" id="defUsuario" style="width:100px" onchange="cambiarFondoInput(this.id)">
 				<option>Seleccione...</option>
 				<option value=1>SÍ</option>
@@ -67,7 +67,10 @@
 		}
 		$respuesta.='
             <tr>                
-                <td style="text-align:center">'.$fila1["usuarioID"].'</td>
+                <td class="sticky1" style="text-align:center">'.$fila1["usuarioID"].'</td>
+				<td class="sticky2" style="text-align:left;!important" id="nombres'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'nombres\',\'nombresAct'.$fila1["usuarioID"].'\')">'.$fila1["nombres"].'</td>	
+                <td style="text-align:left;!important" id="apellidos'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'apellidos\',\'apellidosAct'.$fila1["usuarioID"].'\')">'.$fila1["apellidos"].'</td>	
+                
 		';
 		if($fila1["foto"]==""){
 			$respuesta.='			
@@ -80,11 +83,9 @@
 		}
 		$respuesta.='
                 <td style="padding:0px 15px; text-align:left;!important" id="tdUsuarioCED'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'usuarioCED\',\'usuarioCEDAct'.$fila1["usuarioID"].'\')">'.$fila1["usuarioCED"].'</td>	
-                <td style="text-align:left;!important" id="nombres'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'nombres\',\'nombresAct'.$fila1["usuarioID"].'\')">'.$fila1["nombres"].'</td>	
-                <td style="text-align:left;!important" id="apellidos'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'apellidos\',\'apellidosAct'.$fila1["usuarioID"].'\')">'.$fila1["apellidos"].'</td>	
-                <td style="text-align:left;!important" id="tdCorreo'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'correo\',\'correoAct'.$fila1["usuarioID"].'\')">'.$fila1["correo"].'</td>
-                <td style="text-align:left;!important" id="tdUsuario'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'usuario\',\'usuarioAct'.$fila1["usuarioID"].'\')">'.$fila1["usuario"].'</td>
-                <td style="text-align:left;!important" id="tdConstrasena'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'contrasena\',\'contrasenaAct'.$fila1["usuarioID"].'\')">'.$fila1["contrasena"].'</td>
+                <td style="text-align:left;!important" id="correo'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'correo\',\'correoAct'.$fila1["usuarioID"].'\')">'.$fila1["correo"].'</td>
+                <td style="text-align:left;!important" id="usuario'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'usuario\',\'usuarioAct'.$fila1["usuarioID"].'\')">'.$fila1["usuario"].'</td>
+                <td style="text-align:left;!important" id="constrasena'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarInputUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'contrasena\',\'contrasenaAct'.$fila1["usuarioID"].'\')">'.$fila1["contrasena"].'</td>
                 <td style="text-align:center;!important" id="responsabilidad'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarSeleccionUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'defUsuario\',\'responsabilidadAct'.$fila1["usuarioID"].'\','.$fila1['defUsuario'].')">'.$responsabilidad.'</td>	
                 <td style="text-align:center;!important" id="permiso'.$fila1["usuarioID"].'"><img style="width:10px;height:10px;!important" title="Click para modificar" src="../appsArt/editarOn.png" onclick="actualizarSeleccionUsuario(this.parentNode.id,'.$fila1["usuarioID"].',\'permiso\',\'permisoAct'.$fila1["usuarioID"].'\','.$fila1['permiso'].')">'.$permiso.'</td>	
                 <td class="img"><img src="../appsArt/eliminarOn.png" title="Eliminar" onclick="eliminarRegistroUsuario('.$fila1["usuarioID"].')"/></td>
